18479d8033696540f53a90869ac4b881631b9fdd
Oleh Prypin <blaxpirit@gmail.com>
2015-02-10 17:35:17 +0200

    Update license

--- src/random.nim
+++ src/random.nim
@@ -1,6 +1,6 @@
 # Copyright (C) 2014-2015 Oleh Prypin <blaxpirit@gmail.com>
 # 
-# This file is part of nim-csfml.
+# This file is part of nim-random.
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
